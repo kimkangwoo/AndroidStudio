90f6397816a3b0f08390931169a62bc6c45acb43
kimkangwoo <kmjkmj0020@gmail.com>
2023-11-13 08:50:23 +0900

    안드로이드 초기화 저장

--- app/src/main/java/com/example/test_a/MainActivity.java
+++ app/src/main/java/com/example/test_a/MainActivity.java
@@ -1,10 +1,8 @@
 package com.example.test_a;
 
-
+import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
