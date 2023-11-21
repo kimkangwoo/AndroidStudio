656fdd1b750317c7e024b13f7ea3220bba864802
201902001 <kmjkmj0020@gmail.com>
2023-11-22 08:17:10 +0900

    초기 세팅

--- app/src/main/java/com/example/test_a/MainActivity.java
+++ app/src/main/java/com/example/test_a/MainActivity.java
@@ -1,14 +1,62 @@
 package com.example.test_a;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+// 만보기 제작
+
 public class MainActivity extends AppCompatActivity {
+
+    private SensorManager sensorManager;
+    private Sensor stepSensor;
+    private int stepCount = 0;
+    private TextView countTV; private TextView goalTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        countTV = (TextView)findViewById(R.id.cnt_txt);
+        goalTV = (TextView) findViewById(R.id.goal_txt);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        if (sensorManager != null) {
+            stepSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+            if (stepSensor != null) {
+                sensorManager.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_NORMAL);
+            }
+        }
+
+    }//----------------------------<onCreate>------------------------
+
+
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
+            stepCount = (int) event.values[0];
+            countTV.setText("Step Count: " + stepCount);
+        }
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy){
+        // no return
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Unregister the sensor listener to save power when the app is in the background
+        sensorManager.unregisterListener(this, stepSensor);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Register the sensor listener again when the app is resumed
+        sensorManager.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 }
