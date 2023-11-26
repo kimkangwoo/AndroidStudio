1ad629f8b298c5c89dc3f38c465b342aacbf8680
201902001 <kmjkmj0020@gmail.com>
2023-11-26 20:26:59 +0900

    액티비티 비활성화 때 인식까지 완성  * 추후 : onDestroy에도 만보계 동작, notification으로 만보계 확인

--- app/src/main/java/com/example/test_a/MainActivity.java
+++ app/src/main/java/com/example/test_a/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.test_a;
 
+
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 
 import android.hardware.Sensor;
@@ -22,6 +27,8 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Calendar;
+
 // 만보기 제작
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
@@ -29,12 +36,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private final static String Tag ="MainActivity";
     private SensorManager sensorManager;
     private Sensor stepCountSensor;
-    int stepCount = 0; // 현재 걸음 수
-    private TextView countTV;
+    private int stepCount;
+    private int mstepCount;// 현재 걸음 수
+    protected TextView countTV;
     private TextView goalTV;
     private Button reset;
 
-    //@RequiresApi(api= Build.VERSION_CODES.Q)
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,25 +56,56 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         stepCountSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
 
-        // permission
+        // permission line -------------------------------------------------------------------------
         if(ContextCompat.checkSelfPermission(this, Manifest.
                 permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_DENIED){
             requestPermissions(new String[]{Manifest.permission.ACTIVITY_RECOGNITION}, 0);
         }
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.SET_ALARM)==PackageManager.PERMISSION_DENIED){
+            requestPermissions(new String[]{Manifest.permission.SET_ALARM}, 0);
+        }
+        if(ContextCompat.checkSelfPermission(this, Manifest.
+                permission.RECEIVE_BOOT_COMPLETED) == PackageManager.PERMISSION_DENIED){
+            requestPermissions(new String[]{Manifest.permission.RECEIVE_BOOT_COMPLETED}, 0);
+        }
+        // time set line ---------------------------------------------------------------------------
 
+        // 코드 실행 시간 설정
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        //  00:00
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+
+        // Create an Intent for your BroadcastReceiver
+        Intent intent = new Intent(this, MyReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this,
+                0,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_MUTABLE);
+
+        // AlarmManager service 가져오기
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        alarmManager.set(AlarmManager.RTC, calendar.getTimeInMillis(), pendingIntent);
+
+        // method ----------------------------------------------------------------------------------
         if(stepCountSensor == null){
             Toast.makeText(this, "No Step Sensor", Toast.LENGTH_LONG).show();
         }
 
+        MyReceiver __ = new MyReceiver(countTV);
         reset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // 리셋 버튼 클릭시 이벤트
-                stepCount = 0;
-                countTV.setText(String.valueOf(stepCount));
+                // 리셋 버튼
+                StepCountPreferenceHelper.saveStepCount(MainActivity.this, 0);
+                countTV.setText(String.valueOf("오늘 걸은 횟수 : "+0));
+;
             }
         });
-    }//----------------------------<onCreate>------------------------
+
+    }//----------------------------<onCreate>-------------------------------------------------------
 
     // - SENSOR_DELAY_NORMAL: 20,000 초 딜레이
     // - SENSOR_DELAY_UI: 6,000 초 딜레이
@@ -76,26 +115,46 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         super.onStart();
         if(stepCountSensor != null){
             sensorManager.registerListener(this,
-                    stepCountSensor,
-                    SensorManager.SENSOR_DELAY_UI);
+                    stepCountSensor, SensorManager.SENSOR_DELAY_GAME);
         }
     }
-
+    public void onPause(){
+        super.onPause();
+        if(sensorManager != null) {
+            sensorManager.registerListener(this,
+                    stepCountSensor, SensorManager.SENSOR_DELAY_GAME);
+            }
+        }
+    @Override
+    public void onResume(){
+        super.onResume();
+        if(sensorManager != null){
+            sensorManager.registerListener(this,
+                    stepCountSensor, SensorManager.SENSOR_DELAY_GAME);
+        }
+    }
+    public void onStop(){
+        super.onStop();
+        if(sensorManager != null){
+            sensorManager.registerListener(this,
+                    stepCountSensor, SensorManager.SENSOR_DELAY_GAME);
+        }
+    }
 
     // 센서 이벤트 리스너 메소드
     // 센서는 동작을 감지 하면 이벤트를 발생하여 onSensorChanged에 값을 전달합니다.
     @Override
     public void onSensorChanged(SensorEvent event){
-        Log.d("StepCounter", "Sensor event received : "+ event.sensor.getType() +"/" +Sensor.TYPE_STEP_DETECTOR+"/"+ event.values[0] + "/"+Sensor.TYPE_STEP_COUNTER);
         if(event.sensor.getType() == Sensor.TYPE_STEP_COUNTER){
-                countTV.setText(String.valueOf(stepCount));
-                goalTV.setText(String.valueOf(event.values[0]));
-                stepCount++;
-        }
-        if(event.sensor.getType() == Sensor.TYPE_STEP_DETECTOR){
-            countTV.setText(String.valueOf(stepCount));
-            goalTV.setText(String.valueOf(event.values[0]));
-            stepCount++;
+            stepCount = StepCountPreferenceHelper.getStepCount(this);
+            if (stepCount < 1) {
+                stepCount = (int) event.values[0];
+                // Save the updated step count to SharedPreferences
+                StepCountPreferenceHelper.saveStepCount(this, (int) event.values[0]);
+            }
+            mstepCount = (int) event.values[0] - stepCount;
+            goalTV.setText(String.valueOf("총 걸은 횟수 : "+event.values[0]));
+            countTV.setText(String.valueOf("오늘 걸은 횟수 : "+mstepCount));
         }
     }
 
@@ -103,5 +162,21 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy){
 
+    }
+}
+class StepCountPreferenceHelper {
+    private static final String PREF_NAME = "StepCountPreferences";
+    private static final String KEY_STEP_COUNT = "stepCount";
+
+    public static void saveStepCount(Context context, int stepCount) {
+        SharedPreferences preferences = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt(KEY_STEP_COUNT, stepCount);
+        editor.apply();
+    }
+
+    public static int getStepCount(Context context) {
+        SharedPreferences preferences = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        return preferences.getInt(KEY_STEP_COUNT, 0);
     }
 }
---
+++ app/src/main/java/com/example/test_a/MyReceiver.java
@@ -0,0 +1,59 @@
+package com.example.test_a;
+
+import static android.app.Service.START_STICKY;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Handler;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import org.w3c.dom.Text;
+
+public class MyReceiver extends BroadcastReceiver {
+    /**
+     * Context : 애플리케이션별 리소스 및 서비스에 액세스하는 방법을 제공
+     * Intent : 애플리케이션의 다른 구성 요소에서 작업을 요청하는데 사용되는 메시징 개체,
+     * 활동이나 서비스를 시작하거나 애플리케이션의 다른 부분이나 다른 애플리케이션에 메시지를 브로드캐스트하는데 사용
+     **/
+    private TextView count;
+    public MyReceiver() {
+    }
+    public MyReceiver(TextView count){
+        this.count = count;
+    }
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // 지정된 시간에 적용할 알고리즘 작성
+        Log.d("my receiver", "만보계 카운트 초기화 성공");
+        resetCounts(context);
+        if (count != null) {
+            count.setText("0");
+        }
+
+    }
+
+    private void resetCounts(Context context) {
+
+        // Get the shared preferences to store counts
+        SharedPreferences preferences = context.getSharedPreferences("StepCountPreferences", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        // Reset the counts to 0
+        editor.putInt("stepCount", 0);
+        editor.apply();
+
+        // Update the UI (you may want to send a broadcast or use other mechanisms to update the UI in your activity)
+        Intent updateIntent = new Intent("STEP_COUNT_UPDATED");
+        LocalBroadcastManager.getInstance(context).sendBroadcast(updateIntent);
+
+
+    }
+}
